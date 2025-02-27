e01802858e78e3154adde1ce937b54bb2b1d437a
Andrey <andreydavi3ao@gmail.com>
2025-02-27 09:21:04 -0300

    Lista de Exercícios - Exercício 1

---
+++ js/exum.js
@@ -0,0 +1,5 @@
+let numeroSorteado = parseInt(Math.random()*100);
+let proximoNumero = numeroSorteado+ 1;
+let anteriorNumero = numeroSorteado - 1;
+
+console.log(`O número sorteado é: ${numeroSorteado}, o próximo número é: ${proximoNumero} e o anterior é: ${anteriorNumero}, o dobro desse número é: ${numeroSorteado*2} e sua metade é: ${numeroSorteado/2}`);
